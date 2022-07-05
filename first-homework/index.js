e45a2ee281f0f7ec9ca0aeb4f8ea568ef0e83cf0
Lucija <lucija.strejcek@fer.hr>
2022-07-05 13:06:46 +0200

    Fix formatting

--- first-homework/index.js
+++ first-homework/index.js
@@ -1,48 +1,50 @@
-const reviewFormElement = document.getElementById('review-form')
-const reviewListElement = document.getElementById('review-list')
+const reviewFormElement = document.getElementById('review-form');
+const reviewListElement = document.getElementById('review-list');
 
-const reviews = [{
-    text: 'Great movie!',
-    rating: 5
-}]
+const reviews = [
+	{
+		text: 'Great movie!',
+		rating: 5,
+	},
+];
 
 renderReviews(reviews);
 
 reviewFormElement.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(reviewFormElement);
-    const reviewText = formData.get("text");
-    const reviewRating = formData.get("rating");
+	event.preventDefault();
+	const formData = new FormData(reviewFormElement);
+	const reviewText = formData.get('text');
+	const reviewRating = formData.get('rating');
 
-    reviews.push({
-        text: reviewText,
-        rating: reviewRating
-    });
+	reviews.push({
+		text: reviewText,
+		rating: reviewRating,
+	});
 
-    renderReviews(reviews);
+	renderReviews(reviews);
 
-    reviewFormElement.reset();
+	reviewFormElement.reset();
 });
 
-function renderReviews(reviews){
-    clearReviews();
-    reviews.forEach(review => {
-        const listElement = document.createElement('li');
-        const reviewRating = document.createElement('span');
-        const reviewText = document.createElement('span');
+function renderReviews(reviews) {
+	clearReviews();
+	reviews.forEach((review) => {
+		const listElement = document.createElement('li');
+		const reviewRating = document.createElement('span');
+		const reviewText = document.createElement('span');
 
-        reviewRating.innerText = review.rating;
-        reviewText.innerText = review.text;
+		reviewRating.innerText = review.rating;
+		reviewText.innerText = review.text;
 
-        reviewRating.insertAdjacentText('beforeend', '/5');
-        listElement.appendChild(reviewText);
-        listElement.appendChild(reviewRating);
-        reviewListElement.appendChild(listElement);
-    });
+		reviewRating.insertAdjacentText('beforeend', '/5');
+		listElement.appendChild(reviewText);
+		listElement.appendChild(reviewRating);
+		reviewListElement.appendChild(listElement);
+	});
 }
 
-function clearReviews(){
-    while(reviewListElement.firstChild){
-        reviewListElement.removeChild(reviewListElement.firstChild);
-    }
+function clearReviews() {
+	while (reviewListElement.firstChild) {
+		reviewListElement.removeChild(reviewListElement.firstChild);
+	}
 }
