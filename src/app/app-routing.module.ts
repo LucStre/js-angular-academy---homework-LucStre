dd34d2d6af38efe3f63417a583fe107c6505d795
Lucija <lucija.strejcek@fer.hr>
2022-07-12 21:27:28 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AllShowsComponent } from './pages/all-shows/all-shows.component';
+import { ShowDetailComponent } from './pages/show-detail/show-detail.component';
+import { TopRatedComponent } from './pages/top-rated/top-rated.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: MainLayoutComponent,
+		children: [
+			{
+				path: '',
+				component: AllShowsComponent,
+			},
+			{
+				path: 'top-rated',
+				component: TopRatedComponent,
+			},
+			{
+				path: 'show/:id',
+				component: ShowDetailComponent,
+			},
+		],
+	},
+	{
+		path: '**',
+		redirectTo: '',
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShowsContainerModule } from './components/shows-container/shows-container.module';
+import { MainLayoutModule } from './layouts/main-layout/main-layout.module';
+import { AllShowsModule } from './pages/all-shows/all-shows.module';
+import { TopRatedModule } from './pages/top-rated/top-rated.module';
+import { ShowDetailModule } from './pages/show-detail/show-detail.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, ShowsContainerModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		ShowsContainerModule,
+		MainLayoutModule,
+		AllShowsModule,
+		TopRatedModule,
+		ShowDetailModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
