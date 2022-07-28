4e7292092ff566f571b6d51318b02f24146c8fa1
Lucija <lucija.strejcek@fer.hr>
2022-07-28 11:12:07 +0200

    Post review

--- src/app/services/reviews/reviews.service.ts
+++ src/app/services/reviews/reviews.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable, tap } from 'rxjs';
+import { IReviewData } from 'src/app/interfaces/review-data.interface';
 import { IReview } from 'src/app/interfaces/review.interface';
 import { IReviews } from 'src/app/interfaces/reviews-data.interface';
 import { AuthService } from '../auth/auth.service';
@@ -9,12 +10,27 @@ import { AuthService } from '../auth/auth.service';
 	providedIn: 'root',
 })
 export class ReviewsService {
-	private readonly reviews$ = new BehaviorSubject<Array<IReview>>([]);
+	private readonly _reviews$ = new BehaviorSubject<Array<IReview>>([]);
+	public readonly reviews$ = this._reviews$.asObservable();
 
 	constructor(private readonly http: HttpClient, private readonly authService: AuthService) {}
 
-	public post(data: IReview): void {
-		console.log(data);
+	public create(data: IReview): void {
+		let newReview: IReview;
+		this.http
+			.post<IReviewData>('https://tv-shows.infinum.academy/reviews', data, this.authService.getOptions())
+			.pipe(map((review) => review.review))
+			.subscribe((review) => {
+				if (review) {
+					newReview = review;
+				}
+			});
+		this.reviews$.pipe(
+			tap((reviews) => {
+				reviews.push(newReview);
+				this._reviews$.next(reviews);
+			}),
+		);
 	}
 
 	public allShow(showid: string): Observable<Array<IReview>> {
@@ -23,7 +39,7 @@ export class ReviewsService {
 			.pipe(
 				map((reviews) => reviews.reviews),
 				tap((reviews) => {
-					this.reviews$.next(reviews);
+					this._reviews$.next(reviews);
 				}),
 			);
 	}
